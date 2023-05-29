7eb745930f31583ccfe6af3784f464243cdf3012
victoriashopify <43687004+EduPucheta@users.noreply.github.com>
2023-05-28 21:58:54 -0300

    commit

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Inicio() {
   return (
     <>
-    <p>Te ayudamos a hacer que tus mangos rindan más 🚀
-    </p>
-    <p> 
-    Compará entre dos opciones de financiación y descubrí como te conviene pagar tu compra teniendo en cuenta la inflación, los impuestos pago con tarjeta y la forma de pago que elijas.
-    </p>
+      <div className="text-white">
+        <p >
+          Te ayudamos a hacer que tus mangos rindan más 🚀
+        </p>
+        <p >
+          Compará entre dos opciones de financiación y descubrí como te conviene
+          pagar tu compra teniendo en cuenta la inflación, los impuestos pago
+          con tarjeta y la forma de pago que elijas.
+        </p>
+      </div>
     </>
-  )
+  );
 }
